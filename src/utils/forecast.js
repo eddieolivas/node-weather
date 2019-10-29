4bec00d7507dd8dd134edf0f855eebf882c5f784
Eddie Olivas <eddie@Eddies-MacBook-Pro.local>
2019-10-28 19:52:24 -0700

    Initial commit.

---
+++ src/utils/forecast.js
@@ -0,0 +1,18 @@
+const request = require('request');
+
+const forecast = (lat, long, callback) => {
+    const url = 'https://api.darksky.net/forecast/10d3d2ec96032fbf897af4042d0d2301/' + lat + ',' + long;
+
+    request({ url, json: true}, (error, {body}) => {
+        if (error) {
+            callback('Unable to connect to weather service!', undefined)
+        } else if (body.error) {
+            callback('Unable to find location. Try another search.', undefined);
+        } else {
+            callback(undefined, body.daily.summary + ' It is currently ' + body.currently.temperature + ' degress out. There is ' + body.currently.precipProbability + '% chance of rain.');
+        }
+    });
+    console.log(url);
+};
+
+module.exports = forecast;
